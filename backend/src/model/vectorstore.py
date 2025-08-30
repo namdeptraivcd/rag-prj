cea5c10fc06bba2a72eb190fb7a5e935a20a2348
namdeptraivcd <namhoang4d@gmail.com>
2025-08-30 09:10:24 +0700

    Upgrade qdrant

--- backend/src/model/vectorstore.py
+++ backend/src/model/vectorstore.py
@@ -1,12 +1,26 @@
 from src.config.config import Config
-from langchain_core.vectorstores import InMemoryVectorStore
+from qdrant_client.models import Distance, VectorParams
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
 from src.utils.utils import read_source, split_text
 config = Config()
 
 class Vector_store:
     def __init__ (self):
-        self.embeddings = config.embeddings
-        self.vector_store = InMemoryVectorStore(self.embeddings)    
+        embeddings = config.embeddings
+        client = QdrantClient(":memory:")
+        vector_size = len(embeddings.embed_query("sample text"))
+
+        if not client.collection_exists("test"):
+            client.create_collection(
+                collection_name ="test",
+                vectors_config = VectorParams(size= vector_size, distance= Distance.COSINE)
+            )
+
+        self.vector_store = QdrantVectorStore(
+            client = client,
+            collection_name = "test",
+            embedding = embeddings)    
 
     def load_web(self, web_path):
         docs = read_source(web_path)
